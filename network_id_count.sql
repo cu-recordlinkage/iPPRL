3f76faa2878aad7a233f5ff5503c631c3f074b33
Michael Kahn <Michael.Kahn@cuanschutz.edu>
2021-05-27 18:37:03 -0600

    Plots working with correctly with labels on x-axes in data density & value density

---
+++ network_id_count.sql
@@ -0,0 +1,5 @@
+select * from aim4.network_id where uid = 7873 and run_id =1
+
+select * from aim4.merged_source ms where ms.uid = 7873
+
+select * from aim4.chd_overall_cast where study_id = 24123 and startdate <='2011-12-31'
